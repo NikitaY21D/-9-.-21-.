de0659e186b1e233b9d83bdaabedb18e92cdcd26
iRU <iRU@DESKTOP-QQM0ECT>
2022-01-23 20:27:51 +0300

    Добавьте файлы проекта.

---
+++ задание 9 Яковлев Н 21Д/задание 9 Яковлев Н 21Д.cpp
@@ -0,0 +1,26 @@
+﻿// задание 9 Яковлев Н 21Д
+//Задание 9
+//Вычислить значениеy(n)  = √1 + √2...√n (sqrt(1 + sqrt(2 + ... + sqrt(n))))
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    double sum;
+    cout << "Enter the number: n = ";
+    cin >> n;
+    sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sqrt(i + sum);
+
+    }
+
+    cout << "Sum  = " << sum;
+
+    return 0;
+}
